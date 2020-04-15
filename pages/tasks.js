f3d0e84e21fe29aded9d4938efe7aac61b21a288
JK <jarekkornowicz@googlemail.com>
2020-04-15 23:14:36 +0200

    task fetch

--- pages/tasks.js
+++ pages/tasks.js
@@ -1,11 +1,32 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import fetch from "node-fetch";
 
-function Tasks() {
+function Tasks({ tasks }) {
     return (
         <Layout>
             <h1>Tasks</h1>
+            <ul>
+                {tasks.map(task => (
+                    <li key={task.id}>
+                        {task.title} <br />
+                        {task.description}
+                    </li>
+                ))}
+            </ul>
         </Layout>
     )
 }
+
+export async function getStaticProps() {
+    const res = await fetch("http://127.0.0.1:8000/app/tasks/tasks/")
+    const tasks = await res.json()
+
+    return {
+        props: {
+            tasks,
+        },
+    }
+}
+
 
 export default Tasks;
